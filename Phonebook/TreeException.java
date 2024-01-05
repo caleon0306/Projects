545d140ac901f042000e7dfe5a5bd957d2e2be30
Connell Leon <143096904+caleon0306@users.noreply.github.com>
2024-01-05 17:27:06 -0500

    Delete Phonebook directory

--- Phonebook/TreeException.java
+++
@@ -1,7 +0,0 @@
-package assg9_leonc22;
-
-public class TreeException extends RuntimeException {
-	  public TreeException(String s) {
-	    super(s);
-	  }  // end constructor
-	} // end TreeException
